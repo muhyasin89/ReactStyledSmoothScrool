821d943c7f4df37a4ef8398b03ad5efe05893351
polyglotengineer89 <polyglotengineer89@gmail.com>
2021-06-22 03:46:22 +0700

    [design/hero_section]add herosection

---
+++ src/components/HeroSections/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import Video from '../../videos/video.mp4'
+import { Button } from '../ButtonElements'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
+
+const HeroSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+            </HeroBg>
+        
+            <HeroContent>
+                <HeroH1>
+                    Virtual Banking made easy
+                </HeroH1>
+                <HeroP>
+                    Sign Up for new Account today
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup" 
+                    onMouseEnter={onHover} 
+                    onMouserLeave={onHover} 
+                    primary='true' 
+                    dark='true'>
+                        Get Started { hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
