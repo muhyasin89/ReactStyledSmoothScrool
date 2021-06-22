8f81c4eef25e7f47ca3a6ce8e8a90c9039d03c49
polyglotengineer89 <polyglotengineer89@gmail.com>
2021-06-22 13:49:42 +0700

    [core/add_data]finish with data

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,18 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premiun Bank',
+    headline: 'Unlimited Transaction with zero fee',
+    description: 'Get access to our exclusive app that \
+    allows you send unlimited transaction without \
+    getting charge any fees',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,23 +1,53 @@
 import React from 'react'
 import { Button } from '../ButtonElements'
-import { InfoContainer, InfoWrapper, InfoRow, Column1, TextWrapper, TopLine, Heading, SubTitle,  BtnWrap} from './InfoElements'
+import { 
+    InfoContainer, 
+    InfoWrapper, 
+    InfoRow, 
+    Column1, 
+    Column2,
+    TextWrapper, 
+    TopLine, 
+    Heading, 
+    SubTitle,  
+    BtnWrap,
+    ImgWrap,
+    Img
+} 
+from './InfoElements'
 
-const InfoSection = () => {
+const InfoSection = ({ 
+    id, 
+    lightBg, 
+    imgStart, 
+    lightText, 
+    darkText, 
+    description, 
+    headline, 
+    buttonLabel, 
+    img, 
+    alt
+}) => {
     return (
         <>
-            <InfoContainer>
+            <InfoContainer lightBg={lightBg} id={id}>
                 <InfoWrapper>
-                    <InfoRow>
+                    <InfoRow imgStart={imgStart}>
                         <Column1>
                             <TextWrapper>
                                 <TopLine>Top Line</TopLine>
-                                <Heading> Heading </Heading>
-                                <SubTitle>SubTitle</SubTitle>
+                                <Heading lightText={lightText}> {headline} </Heading>
+                                <SubTitle darkText={darkText}>{description}</SubTitle>
                                 <BtnWrap>
-                                    <Button to="home">Button</Button>
+                                    <Button to="home">{ buttonLabel }</Button>
                                 </BtnWrap>
                             </TextWrapper>
                         </Column1>
+                        <Column2>
+                            <ImgWrap>
+                                <Img src={img} alt={alt} />
+                            </ImgWrap>
+                        </Column2>
                     </InfoRow>
                 </InfoWrapper>
             </InfoContainer>
