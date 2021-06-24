23181bd781fb4e30ce5dfa6b568e635c56f1045d
polyglotengineer89 <polyglotengineer89@gmail.com>
2021-06-24 07:42:03 +0700

    [design/add_footer]finish footer components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,8 +6,22 @@ import {
     FooterLinksWrapper,
     FooterLinkItems,
     FooterLinkTitle,
-    FooterLink
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    WebsiteRights,
+    SocialIcons,
+    SocialIconsLink
 } from './FooterElements'
+
+import { 
+    FaFacebook,
+    FaInstagram,
+    FaTwitter,
+    FaYoutube,
+    FaLinkedin
+ } from 'react-icons/fa'
 
 const Footer = () => {
     return (
@@ -17,35 +31,71 @@ const Footer = () => {
                     <FooterLinksWrapper>
                         <FooterLinkItems>
                             <FooterLinkTitle>
-                                <FooterLink to="/signin">About Us</FooterLink>
-                                <FooterLink to="/signin">How it works</FooterLink>
-                                <FooterLink to="/signin">Testimonials</FooterLink>
-                                <FooterLink to="/signin">Investors</FooterLink>
-                                <FooterLink to="/signin">Terms Services</FooterLink>
+                                <FooterLink to="/">About Us</FooterLink>
+                                <FooterLink to="/">How it works</FooterLink>
+                                <FooterLink to="/">Testimonials</FooterLink>
+                                <FooterLink to="/">Investors</FooterLink>
+                                <FooterLink to="/">Terms Services</FooterLink>
                             </FooterLinkTitle>
                         </FooterLinkItems>
                         <FooterLinkItems>
                             <FooterLinkTitle>
-                                <FooterLink to="/signin">About Us</FooterLink>
-                                <FooterLink to="/signin">How it works</FooterLink>
-                                <FooterLink to="/signin">Testimonials</FooterLink>
-                                <FooterLink to="/signin">Investors</FooterLink>
-                                <FooterLink to="/signin">Terms Services</FooterLink>
+                                <FooterLink to="/">Video</FooterLink>
+                                <FooterLink to="/">Submit Video</FooterLink>
+                                <FooterLink to="/">Ambasador</FooterLink>
+                                <FooterLink to="/">Agency</FooterLink>
+                                <FooterLink to="/">Influencer</FooterLink>
                             </FooterLinkTitle>
                         </FooterLinkItems>
 
                         <FooterLinkItems>
                             <FooterLinkTitle>
-                                <FooterLink to="/signin">About Us</FooterLink>
-                                <FooterLink to="/signin">How it works</FooterLink>
-                                <FooterLink to="/signin">Testimonials</FooterLink>
-                                <FooterLink to="/signin">Investors</FooterLink>
-                                <FooterLink to="/signin">Terms Services</FooterLink>
+                                <FooterLink to="/">Contact Us</FooterLink>
+                                <FooterLink to="/">Contact</FooterLink>
+                                <FooterLink to="/">Support</FooterLink>
+                                <FooterLink to="/">Destination</FooterLink>
+                                <FooterLink to="/">Sponsorship</FooterLink>
+                            </FooterLinkTitle>
+                        </FooterLinkItems>
+
+                        <FooterLinkItems>
+                            <FooterLinkTitle>
+                                <FooterLink to="/">Social Media</FooterLink>
+                                <FooterLink to="/">Instagram</FooterLink>
+                                <FooterLink to="/">Facebook</FooterLink>
+                                <FooterLink to="/">Youtube</FooterLink>
+                                <FooterLink to="/">Twitter</FooterLink>
                             </FooterLinkTitle>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
-                    
                 </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to="/">
+                            dolla
+                        </SocialLogo>
+                        <WebsiteRights>
+                                dolla {new Date().getFullYear()} All Right Reserve
+                        </WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconsLink href='/' target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconsLink>
+                            <SocialIconsLink href='/' target="_blank" aria-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconsLink>
+                            <SocialIconsLink href='/' target="_blank" aria-label="Youtube">
+                                <FaYoutube />
+                            </SocialIconsLink>
+                            <SocialIconsLink href='/' target="_blank" aria-label="Twitter">
+                                <FaTwitter />
+                            </SocialIconsLink>
+                            <SocialIconsLink href='/' target="_blank" aria-label="Linkedin">
+                                <FaLinkedin />
+                            </SocialIconsLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
             </FooterWrap>
         </FooterContainer>
     )
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { FaBars } from 'react-icons/fa'
-import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements'; 
+import { IconContext } from 'react-icons/lib';
+import { animateScroll as scroll } from 'react-scroll';
+import { 
+    Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks, 
+    NavBtn, 
+    NavBtnLink 
+} from './NavbarElements'; 
 
 const Navbar = ({toggle}) => {
+    const [scrollNav,setScrolNav] = useState(false)
+
+    const changeNav = () =>{
+        if(window.scrollY >= 80){
+            setScrolNav(true)
+        }else{
+            setScrolNav(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNav)
+    }, [])
+
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    }
+
     return (
         <>
-            <Nav>
+        <IconContext.Provider value={{ color: 'red'}}>
+            <Nav scrollNav={scrollNav}>
                 <NavbarContainer>
-                    <NavLogo to='/'>Rupiah</NavLogo>
+                    <NavLogo to='/' onClick={toggleHome}>Rupiah</NavLogo>
                     <MobileIcon onClick={toggle}>
                         <FaBars />
                     </MobileIcon>
@@ -30,6 +61,7 @@ const Navbar = ({toggle}) => {
                     </NavBtn>
                 </NavbarContainer>
             </Nav>
+            </IconContext.Provider>
         </>
     )
 }
